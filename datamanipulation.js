2e7e2b4ca1ae214cdca537d11064dda8bccfd95f
satyendra123 <Singhsatyendra885@gmail.com>
2025-05-25 22:20:41 +0530

    Update datamanipulation.js

--- datamanipulation.js
+++ datamanipulation.js
@@ -10,8 +10,8 @@ console.log(second_data);                                             [ session=
 extract_data = second_data.split('=')[1];                             [session
 console.log(extract_data)                                              ytzek22yw8c41jj5qtlvqz3n20vmecgo
                                                                       ]
-EX-2                                                        EX-3 
-const currenttime = new Date();                                  const currenttime = new Date();
-const newtime = currenttime.toLocaleString();                    const isoString = currenttime.toISOString();
-const datetime = newtime.split(',')[1]                           const new_date = isoString.split('T')[0];
-console.log(datetime)                                            console.log(new_date);
+EX-2                                                        EX-3                                                          Ex-4
+const currenttime = new Date();                                  const currenttime = new Date();                               const currenttime = new Date(); 
+const newtime = currenttime.toLocaleString();                    const isoString = currenttime.toISOString();                  const newtime = currenttime.toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
+const datetime = newtime.split(',')[1]                           const new_date = isoString.split('T')[0];                     const datetime = newtime.split(',')[1].trim();
+console.log(datetime)                                            console.log(new_date);                                        console.log(datetime);
